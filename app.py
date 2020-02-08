4b4d3ac51cd20610a14a76f146acd10ad84d9989
Julie Loisel <julie.loiselnaim@gmail.com>
2020-02-08 16:58:07 +0100

    server

--- app.py
+++ app.py
@@ -14,6 +14,8 @@ from fromage import *
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+
 #colors ={'background':'#111111','text':'#7FDBFF'}
 colors = {
     'background': '#111111',
