a1f8562b9f57421a5bd4b2b8cea42cb922ad616e
julie-loisel <Julie.loiselnaim@gmail.com>
2020-02-08 14:51:31 +0100

    Add files via upload

---
+++ sampling.py
@@ -0,0 +1,66 @@
+#calculs
+import numpy as np 
+import pandas as pd 
+from fromage import utils,objects,calculs
+from scipy.stats import linregress
+
+def generate_data(size,palette,produit,Vfr=0.31,proba_rupture=0.5,dt=30):
+    """Fonction qui génère des profils de température, avec une certaine probabilité
+    d'observer une rupture. Chaque profil est calculé grâce au modèle thermique 
+    à partir d'un profil de T_air simulé, et une zone de la palette est choisie
+    aléatoirement.
+    Parameters:
+    -----------
+    size : nombre de profils à simuler
+    palette : objet palette
+        la palette que l'on considère 
+    produit : objet produit
+        le produit qui est dans la palette
+    proba_rupture : probabilité d'observer une rupture 
+    Vfr : vitesse frontale
+    dt : pas de temps
+
+    Returns:
+    --------
+    data: pd.DataFrame
+    '''
+    """
+    
+    id=0
+    data0=[]
+    for nb_it in range(size):
+        rupture=(np.random.random()<proba_rupture)*1
+        if rupture:
+            T,T_air=utils.constructT_air_avec_rupture()
+        else:
+            T,T_air=utils.constructT_air_sans_rupture()
+        Tprod,T_az=calculs.calcul_profils(palette,produit,T_air,dt,Vfr)
+        
+        k=np.random.randint(1,19)
+        data=pd.DataFrame(columns=['T','T_air','No_Produit','Rupture','id','T_prod'])
+        len_T=len(T_air)
+        data["T_air"]=T_air
+        data["T"]=T
+        data["id"]=nb_it
+        data["T_air"]=T_air
+        data["Rupture"]=rupture
+        data["No_Produit"]=k
+        data["T_prod"]=Tprod[k-1,:]
+        data0.append(data)
+        
+    return pd.concat(data0).set_index(["id"])
+
+def slope(x,y):
+    return linregress(x,y)[0]
+
+def vectorize_data(df):
+    index = df.index.unique()
+    data=pd.DataFrame(columns=['T', 'T_air', 'No_Produit', 'Rupture', 'T_prod'],index=index)
+    for i in index:
+        data.loc[i][["No_Produit","Rupture"]]=df.loc[i][["No_Produit","Rupture"]].values[0]
+        data.loc[i]["T"]=df.loc[i]["T"].values
+
+        data.loc[i]["T"]=df.loc[i]["T"].values
+        data.loc[i]["T_air"]=df.loc[i]["T_air"].values
+        data.loc[i]["T_prod"]=df.loc[i]["T_prod"].values
+    return data
---
+++ utils.py
@@ -0,0 +1,128 @@
+#utils
+import numpy as np
+def construct_Sx_Sy(config,C_vent=1,S_h1=8.2e-4*2,S_h2=1.62e-3+6.9e-4*2,\
+                    S_h3=7e-4+1.1e-3,S_h4=7e-4/2,S_h5=7e-4/4,S_h6=8.2e-4,S_h7=1.62e-3):
+    
+    if config==1:
+        S_h1,S_h2,S_h3 = C_vent*S_h1,C_vent*S_h2,C_vent*S_h3
+        Sy = np.array([[S_h1,1,S_h1,1,S_h1,1,S_h1],
+               [S_h2,1,S_h2,1,S_h2,1,S_h2],
+               [S_h1,1,S_h1,1,S_h1,1,S_h1]])
+        Sx=np.array([[1., 1., 1., 1., 1., 1.],
+               [1., 1., 1., 1., 1., 1.],
+               [1., 1., 1., 1., 1., 1.],
+               [0., S_h3, 0., S_h3, 0., S_h3]])
+    if config==2:
+        S_h4,S_h5,S_h6,S_h7= C_vent*S_h4,C_vent*S_h5,\
+                            C_vent*S_h6,C_vent*S_h7
+        Sy=np.array([[S_h4,1,1,S_h5,1,1,S_h4],
+                       [S_h4,1,1,S_h5,1,1,S_h4],
+                       [S_h4,1,1,S_h5,1,1,S_h4]])
+        Sx=np.array([[1., 1., 1., 1., 1., 1.],
+               [S_h6, S_h7,S_h6, S_h6, S_h7, S_h6],
+               [1., 1., 1., 1., 1., 1.],
+               [S_h6, S_h7,S_h6, S_h6, S_h7, S_h6]])
+    return Sx,Sy
+def construct_Mcx_Mcy(config,Ce=0.07,Ci=0.0256,Cc=0.0128):
+    if config==1:
+        
+        Mcy = np.array([[0,Ci,Cc,Ci,Cc,Ci,Ce],
+               [0,Ci,Cc,Ci,Cc,Ci,Ce],
+               [0,Ci,Cc,Ci,Cc,Ci,Ce]])
+        Mcx = np.array([np.ones(6)*0,#ou Cc ???
+               np.ones(6)*Ci,
+               np.ones(6)*Ci,
+               np.ones(6)*Ce])
+    
+    return Mcx,Mcy
+def construct_T_air(dt,tr1,T1,tr2=0,T2=0,tr3=0,T3=0):
+    t00=240
+    t0=640
+    Ta=0
+    T_0=np.ones(int(t0*60/dt+1))*Ta
+    T_00=np.ones(int(t00*60/dt))*Ta
+    T_r1=np.ones(int(tr1*60/dt))*T1#T°C Rupture 1
+    T_r2=np.ones(int(tr2*60/dt))*T2#T°C Rupture 1
+    T_r3=np.ones(int(tr3*60/dt))*T3#T°C Rupture 1
+    t_tot=t0+tr1+tr2+t00+t00+tr2+t00+t00+tr3+17*t00;
+    T=np.arange(0,60*t_tot+1,dt)
+    #T_air=np.concatenate([T_0,T_r1,T_00,T_00,T_00,T_00,T_r2,T_00,T_00,\
+           #T_00,T_00,T_r2,T_00,T_00,T_00,T_00,T_r3,T_00,T_00,T_00,T_00,T_00,T_00,T_00,T_00,T_00])
+    T_air=np.concatenate([T_0,T_r1,T_00,T_00,T_00,T_00,T_r2,T_00,T_00,\
+           T_00,T_00,T_r2,T_00,T_00,T_00,T_00,T_r3,T_0,T_0])
+    return T,T_air 
+
+def construct_T_air_bis(dt,Ta,debut_t1,durees,pauses,temperatures):
+    t_tot=debut_t1
+    t1=debut_t1*60/dt
+    T_air=np.ones(int(t1))*Ta
+    for duree,pause,temperature in zip(durees,pauses,temperatures):
+        t_tot=t_tot+duree+pause
+        duree=int(duree*60/dt)
+        pause=int(pause*60/dt)
+        T_air=np.concatenate([T_air,np.ones(duree)*temperature,np.ones(pause)*Ta])
+    
+    T=np.arange(0,60*t_tot,dt)
+    
+    return T,T_air 
+
+def constructT_air_sans_rupture(dt=30):
+    
+    T_plateforme,t_plateforme=np.random.normal(loc=3.4,scale=1.4),np.random.exponential(1.05)*3600*24
+    T_camion,t_camion=np.random.normal(loc=3.3,scale=1.2),np.random.exponential(0.25)*3600*24
+    T_chambre,t_chambre=np.random.normal(loc=3.4,scale=1.4),np.random.exponential(1.05)*3600*24
+    T_air=np.array([])
+    t_tot=0
+    for T,t in zip([T_plateforme,T_camion,T_chambre],[t_plateforme,t_camion,t_chambre]):
+        T_air=np.concatenate([T_air,T*np.ones(int(t/dt))])
+        t_tot=t_tot+int(t/dt)*dt
+    T=np.arange(0,t_tot,dt)
+    return T,T_air
+
+def constructT_air_avec_rupture(dt=30,lambda_rupture=2.05):
+    """lambda_rupture: paramètre de la loi exponentielle qui génère la durée de la rupture
+    """
+    T_plateforme,t_plateforme=np.random.normal(loc=3.4,scale=1.4),np.random.exponential(1.05)*3600*24
+    T_camion,t_camion=np.random.normal(loc=3.3,scale=1.2),np.random.exponential(0.25)*3600*24
+    T_chambre,t_chambre=np.random.normal(loc=3.4,scale=1.4),np.random.exponential(1.05)*3600*24
+    T_air=np.array([])
+    t_tot=0
+    rupture=np.random.randint(0,2)
+    for T,t,r in zip([T_plateforme,T_camion,T_chambre],[t_plateforme,t_camion,t_chambre],range(3)):
+        Temps=int(t/dt)
+        T_air=np.concatenate([T_air,T*np.ones(Temps)])
+        t_tot=t_tot+Temps*dt
+        if rupture==r:
+            temp_rupture=np.random.randint(6,25)
+            Temps_rupture=np.random.exponential(lambda_rupture)*3600
+            Temps=int(Temps_rupture/dt)
+            T_air=np.concatenate([T_air,temp_rupture*np.ones(Temps)])
+            t_tot=t_tot+Temps*dt
+    T=np.arange(0,t_tot,dt)
+    return T,T_air
+    
+def init_pression(palette,produit,Vfr=0.31,e=0,f=0,Pa=1):
+    I,J=palette.I,palette.J
+    rho=produit.rho
+    if Vfr>0:
+        Pa = 0.5*rho*Vfr**2
+    Pc = np.zeros((I+2,J+2))
+    Tc = np.zeros((I+2,J+2))
+    for i in range(I):
+        Pc[i+1,0]=Pa
+    for j in range(J):
+        Pc[I+1,j]=Pa*(f+(e-f)*(2*J+1-2*j)/(2*J))
+    return Pc,Pa
+
+def calcul_ij(k,I,J):
+    i=int(k//J+(1*(k%J!=0)))-1
+    j =int(k%J + (k%J==0)*(k/(k/J)))-1
+    return i,j
+
+def init_champs_temperature(Qx,Qy,produit,Cp=1005):
+    rho=produit.rho
+    Qxp=np.maximum(Qx,0)*rho*Cp
+    Qxm=-np.minimum(Qx,0)*rho*Cp
+    Qyp=np.maximum(Qy,0)*rho*Cp
+    Qym=-np.minimum(Qy,0)*rho*Cp
+    return Qxp,Qxm,Qyp,Qym
