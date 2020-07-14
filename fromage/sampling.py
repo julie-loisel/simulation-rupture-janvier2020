d63d11df4d214083002de9240ae8af36e1bfe5d9
Julie Loisel <julie.loiselnaim@gmail.com>
2020-07-14 17:40:42 +0200

    server

--- fromage/sampling.py
+++ fromage/sampling.py
@@ -30,11 +30,11 @@ def generate_data(size,palette,produit,Vfr=0.31,proba_rupture=0.5,dt=30):
     data0=[]
     for nb_it in range(size):
         rupture=(np.random.random()<proba_rupture)*1
-        circuit=np.rupturendom.randint(8,size=1)
+        circuit=np.random.randint(8,size=1)
         if rupture:
-            T,T_air,liste_stages=utils.constructT_air_avec_rupture_chaine(chaine=chaine(circuit=circuit))
+            T,T_air,liste_stages=utils.constructT_air_avec_rupture_chaine(chaine=objects.chaine(circuit=circuit))
         else:
-            T,T_air,liste_stage=utils.constructT_air_sans_rupture_chaine(chaine=chaine(circuit=circuit))
+            T,T_air,liste_stage=utils.constructT_air_sans_rupture_chaine(chaine=objects.chaine(circuit=circuit))
         Tprod,T_az=calculs.calcul_profils(palette,produit,T_air,dt,Vfr)
         
         k=np.random.randint(1,19)
@@ -59,8 +59,6 @@ def vectorize_data(df):
     data=pd.DataFrame(columns=['T', 'T_air', 'No_Produit', 'Rupture', 'T_prod'],index=index)
     for i in index:
         data.loc[i][["No_Produit","Rupture"]]=df.loc[i][["No_Produit","Rupture"]].values[0]
-        data.loc[i]["T"]=df.loc[i]["T"].values
-
         data.loc[i]["T"]=df.loc[i]["T"].values
         data.loc[i]["T_air"]=df.loc[i]["T_air"].values
         data.loc[i]["T_prod"]=df.loc[i]["T_prod"].values
