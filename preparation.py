4bd89e7a2d6d29639455c7992262c86addc3f7e7
loisel <julie.loiselnaim@gmail.com>
2020-07-16 11:49:22 +0200

    modif ccbreak bool

--- preparation.py
+++ preparation.py
@@ -12,25 +12,24 @@ t0 = time.time()
 circuit=np.random.randint(8,size=1)
 palette = palette()
 produit = produit()
-for i in range(100):
+for i in range(1000):
     chaine = objects.chaine(circuit=circuit)
-
     T,T_air,liste_stages,ccbreak_bool=constructT_air_avec_rupture_chaine(chaine=chaine)
     Tprod,T_az=calculs.calcul_profils(palette,produit,T_air)
-print(t0-time.time())
+print(time.time()-t0)
 #####
 
-Tprod0_data = validate_series(pd.DataFrame(Tprod[0,:].T,index=pd.DatetimeIndex(T)))
-T_az0_data = validate_series(pd.DataFrame(T_az[0,:].T,index=pd.DatetimeIndex(T)))
+#Tprod0_data = validate_series(pd.DataFrame(Tprod[0,:].T,index=pd.DatetimeIndex(T)))
+#T_az0_data = validate_series(pd.DataFrame(T_az[0,:].T,index=pd.DatetimeIndex(T)))
 
 
-threshold_produit = ThresholdAD(high=5, low=0)
-threshold_air = ThresholdAD(high=5, low=0)
+#threshold_produit = ThresholdAD(high=5, low=0)
+#threshold_air = ThresholdAD(high=5, low=0)
 
-anomalies_produit = threshold_produit.detect(Tprod0_data)
-anomalies_air = threshold_air.detect(T_az0_data)
+#anomalies_produit = threshold_produit.detect(Tprod0_data)
+#anomalies_air = threshold_air.detect(T_az0_data)
 #print(recall(anomalies_produit,anomalies_air))
 #print(len((T_air)))
 #print(anomalies_produit)
 #print(to_events(anomalies_produit))
-print(ccbreak_bool)
+#print(ccbreak_bool)
